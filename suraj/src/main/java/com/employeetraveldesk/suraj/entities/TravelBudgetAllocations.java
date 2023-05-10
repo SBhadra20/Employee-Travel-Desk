a4f60cb2d0b5415f636af419fa06d0dd02a9c4d4
Suraj Bhadra <70887781+SBhadra20@users.noreply.github.com>
2023-05-10 08:57:09 +0530

    Update TravelBudgetAllocations.java

--- suraj/src/main/java/com/employeetraveldesk/suraj/entities/TravelBudgetAllocations.java
+++ suraj/src/main/java/com/employeetraveldesk/suraj/entities/TravelBudgetAllocations.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="TravelBudgetAllocations")
+@Table(name="travelbudgetallocations")
 @NoArgsConstructor
 @Getter
 @Setter
